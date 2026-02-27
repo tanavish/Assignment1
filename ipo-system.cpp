1bb3fdb4daa1bfae77ba0b1bc59d447b44ecf82e
tanavish <tanavishf@gmail.com>
2026-02-27 11:30:09 +0500

    Create ipo-system.cpp

---
+++ ipo-system.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number, square;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    square = number * number;
+
+    cout << "Square is: " << square;
+
+    return 0;
+}
